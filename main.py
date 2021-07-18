a7cd28f39a2d0d30da178aebe1952d5f6a2280a0
YungIshimura <83189636+YungIshimura@users.noreply.github.com>
2021-07-18 12:46:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import requests
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+load_dotenv()
+import argparse
+import sys
+
+
+API_KEY = os.getenv("BITLY_API_KEY")
+
+def add_link():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user_link', nargs = '+')
+
+    return parser
+
+
+def check_link(api_key, user_link):
+    link_check = "https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
+    parsed_user_link = urlparse(user_link)
+    headers = {"Authorization": f"Bearer {api_key}"}
+    response = requests.get(
+        link_check.format(bitlink=f"{parsed_user_link.netloc}{parsed_user_link.path}"),
+        headers=headers,
+    )
+    check = response.ok
+    return check
+
+
+def shorten_link(api_key, user_link):
+    bitlink_url = "https://api-ssl.bitly.com/v4/shorten"
+    headers = {"Authorization": f"Bearer {api_key}"}
+    payload = {"long_url": user_link}
+    response = requests.post(bitlink_url, headers=headers, json=payload)
+    response.raise_for_status()
+    bitlink = response.json()
+    return bitlink["id"]
+
+
+def count_clicks(api_key, user_link):
+    click_url = "https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
+    parsed_bitlink = urlparse(user_link)
+    headers = {"Authorization": f"Bearer {api_key}"}
+    payload = {
+        "unit": "day",
+        "units": -1,
+    }
+    response = requests.get(
+        click_url.format(bitlink=f"{parsed_bitlink.netloc}{parsed_bitlink.path}"),
+        params=payload,
+        headers=headers,
+    )
+    response.raise_for_status()
+    count_clicks = response.json()
+    return count_clicks["total_clicks"]
+
+
+if __name__ == "__main__":
+    parser = add_link()
+    namespace = parser.parse_args(sys.argv[1:])
+    for user_link in namespace.user_link:
+        check = check_link(API_KEY, user_link)
+        try:
+            if not check:
+                print("Битлинк: ", shorten_link(API_KEY, user_link))
+            else:
+                print(f"По вашей ссылке прошли: {count_clicks(API_KEY, user_link)} раз (а)")
+        except requests.exceptions.HTTPError:
+                print("Ссылка введена неверно ")
